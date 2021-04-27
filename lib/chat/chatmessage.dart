c381b398abf5676f88ac1e7e612b18a7d3e64d8f
silentl007 <xtreemsilentl@gmail.com>
2021-04-27 15:59:45 +0100

    logged in user messages stays at the right, others to the left

--- lib/chat/chatmessage.dart
+++ lib/chat/chatmessage.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
+  final int index;
+  final Map<String, dynamic> data;
+  ChatMessage({this.data, this.index});
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double maxWidth300 = size.height * 0.3755;
+    double padding10 = size.height * 0.01251;
+    double padding12 = size.height * 0.015;
+    double padding6 = size.height * 0.0075;
+    double font12 = size.height * 0.015;
+    double size40 = size.height * 0.05;
     return Container(
-      
-    );
+        child: Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.only(
+            left: padding10,
+            right: padding10,
+          ),
+          padding: EdgeInsets.symmetric(
+            horizontal: padding10,
+            vertical: padding12,
+          ),
+          constraints: BoxConstraints(maxWidth: maxWidth300),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+          ),
+          child: Text(data['value']),
+        ),
+      ],
+    ));
   }
 }
--- lib/chat/messagewall.dart
+++ lib/chat/messagewall.dart
@@ -1,5 +1,7 @@
+import 'package:BetSlipCode/chat/chatmessage.dart';
 import 'package:BetSlipCode/chat/chatmessageother.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:BetSlipCode/chat/messagewall.dart';
 
@@ -19,9 +21,17 @@ class MessageWall extends StatelessWidget {
     return ListView.builder(
       itemCount: messages.length,
       itemBuilder: (context, index) {
+        final data = messages[index].data();
+        final user = FirebaseAuth.instance.currentUser;
+        if (user != null && user.uid == data['author_id']) {
+          return ChatMessage(
+            index: index,
+            data: data,
+          );
+        }
         return ChatMessageOther(
           index: index,
-          data: messages[index].data(),
+          data: data,
           showAvatar: shouldShowAvatar(index),
         );
       },
